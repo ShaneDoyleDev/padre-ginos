e09f8d67f719e11ada09094972a90b008806a06e
Shane Doyle <shanedoyle1989@gmail.com>
2025-01-04 13:27:36 +0000

    import Pizza component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
-import React from "react";
 import { createRoot } from "react-dom/client";
+import Pizza from "./Pizza";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h1>Padre Gino's Pizza – Order Now</h1>
+      <Pizza name="Pepperoni" description="Mozzarella Cheese, Pepperoni" />
+      <Pizza
+        name="The Hawaiian Pizza"
+        description="Sliced Ham, Pineapple, Mozzarella Cheese"
+      />
+      <Pizza
+        name="The Big Meat Pizza"
+        description="Bacon, Pepperoni, Italian Sausage, Chorizo Sausage"
+      />
+    </div>
+  );
 };
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(React.createElement(App));
+root.render(<App />);
